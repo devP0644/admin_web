a560f3951b13d9ec8569250c4b8ade3130ed5f2d
devP0644 <phg0644@naver.com>
2020-04-07 18:33:14 +0900

    create partner restful api

---
+++ src/main/java/kr/co/fastcampus/admin/controller/api/PartnerApiController.java
@@ -0,0 +1,41 @@
+package kr.co.fastcampus.admin.controller.api;
+
+import kr.co.fastcampus.admin.ifs.CrudInterface;
+import kr.co.fastcampus.admin.model.network.Header;
+import kr.co.fastcampus.admin.model.network.request.PartnerApiRequest;
+import kr.co.fastcampus.admin.model.network.response.PartnerApiResponse;
+import kr.co.fastcampus.admin.service.PartnerApiLogicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/partner")
+public class PartnerApiController implements CrudInterface<PartnerApiRequest, PartnerApiResponse> {
+
+    @Autowired
+    PartnerApiLogicService partnerApiLogicService;
+
+    @Override
+    @PostMapping("")
+    public Header<PartnerApiResponse> create(@RequestBody Header<PartnerApiRequest> request) {
+        return partnerApiLogicService.create(request);
+    }
+
+    @Override
+    @GetMapping("{id}")
+    public Header<PartnerApiResponse> read(@PathVariable Long id) {
+        return partnerApiLogicService.read(id);
+    }
+
+    @Override
+    @PutMapping("")
+    public Header<PartnerApiResponse> update(@RequestBody Header<PartnerApiRequest> request) {
+        return partnerApiLogicService.update(request);
+    }
+
+    @Override
+    @DeleteMapping("{id}")
+    public Header delete(@PathVariable Long id) {
+        return partnerApiLogicService.delete(id);
+    }
+}
---
+++ src/main/java/kr/co/fastcampus/admin/service/PartnerApiLogicService.java
@@ -0,0 +1,101 @@
+package kr.co.fastcampus.admin.service;
+
+import kr.co.fastcampus.admin.ifs.CrudInterface;
+import kr.co.fastcampus.admin.model.entity.Partner;
+import kr.co.fastcampus.admin.model.network.Header;
+import kr.co.fastcampus.admin.model.network.request.PartnerApiRequest;
+import kr.co.fastcampus.admin.model.network.response.PartnerApiResponse;
+import kr.co.fastcampus.admin.repository.CategoryRepository;
+import kr.co.fastcampus.admin.repository.PartnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class PartnerApiLogicService implements CrudInterface<PartnerApiRequest, PartnerApiResponse> {
+
+    @Autowired
+    PartnerRepository partnerRepository;
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Override
+    public Header<PartnerApiResponse> create(Header<PartnerApiRequest> request) {
+        PartnerApiRequest partnerApiRequest = request.getData();
+
+        Partner partner = Partner.builder()
+                .id(partnerApiRequest.getId())
+                .name(partnerApiRequest.getName())
+                .status(partnerApiRequest.getStatus())
+                .address(partnerApiRequest.getAddress())
+                .callCenter(partnerApiRequest.getCallCenter())
+                .partnerNumber(partnerApiRequest.getPartnerNumber())
+                .businessNumber(partnerApiRequest.getBusinessNumber())
+                .ceoName(partnerApiRequest.getCeoName())
+                .registeredAt(LocalDateTime.now())
+                .category(categoryRepository.getOne(partnerApiRequest.getCategoryId()))
+                .build();
+
+        Partner newPartner = partnerRepository.save(partner);
+
+        return response(newPartner);
+    }
+
+    @Override
+    public Header<PartnerApiResponse> read(Long id) {
+        return partnerRepository.findById(id)
+                .map(partner -> response(partner))
+                .orElseGet(()->Header.ERROR("데이터 없음"));
+    }
+
+    @Override
+    public Header<PartnerApiResponse> update(Header<PartnerApiRequest> request) {
+        PartnerApiRequest partnerApiRequest = request.getData();
+
+        return partnerRepository.findById(partnerApiRequest.getId())
+                .map(partner -> {
+                    partner.setId(partnerApiRequest.getId())
+                            .setName(partnerApiRequest.getName())
+                            .setAddress(partnerApiRequest.getAddress())
+                            .setCallCenter(partnerApiRequest.getCallCenter())
+                            .setPartnerNumber(partnerApiRequest.getPartnerNumber())
+                            .setBusinessNumber(partnerApiRequest.getBusinessNumber())
+                            .setCeoName(partnerApiRequest.getCeoName());
+
+                    return partner;
+                })
+                .map(partner -> partnerRepository.save(partner))
+                .map(partner -> response(partner))
+                .orElseGet(()->Header.ERROR("데이터 없음"));
+    }
+
+    @Override
+    public Header delete(Long id) {
+
+        return partnerRepository.findById(id)
+                .map(partner -> {
+                    partnerRepository.delete(partner);
+                    return Header.OK();
+                })
+                .orElseGet(()->Header.ERROR("데이터 없음"));
+    }
+
+    private Header<PartnerApiResponse> response(Partner partner) {
+        PartnerApiResponse partnerApiResponse = PartnerApiResponse.builder()
+                .id(partner.getId())
+                .name(partner.getName())
+                .status(partner.getStatus())
+                .address(partner.getAddress())
+                .callCenter(partner.getCallCenter())
+                .partnerNumber(partner.getPartnerNumber())
+                .businessNumber(partner.getBusinessNumber())
+                .ceoName(partner.getCeoName())
+                .registeredAt(partner.getRegisteredAt())
+                .unregisteredAt(partner.getUnregisteredAt())
+                .categoryId(partner.getCategory().getId())
+                .build();
+
+        return Header.OK(partnerApiResponse);
+    }
+}
